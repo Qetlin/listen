1df8fff3f797d78351fa18e0f50fcfed9d0a4522
Qetlin <brettleahy@gmail.com>
2014-05-13 17:33:18 -0400

    post 'timer' rake

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140513211214) do
+ActiveRecord::Schema.define(version: 20140513213042) do
 
   create_table "geos", force: true do |t|
     t.datetime "created_at"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20140513211214) do
   end
 
   create_table "sms", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "timers", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
