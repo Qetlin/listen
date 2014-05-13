a90e3f9ac8ae06d6210b7bdbad38e6181e68c75b
Qetlin <brettleahy@gmail.com>
2014-05-13 17:15:46 -0400

    post rake

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140513211214) do
+
+  create_table "geos", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "images", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "sms", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
